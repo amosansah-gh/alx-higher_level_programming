bdcb872a3e9d48ca40279b573f4a422cb26459ec
root <amosansah44@gmail.com>
2024-01-04 11:33:50 -0800

    Python import and modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """call each of the imported functions && perform the basic arithemetics of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
